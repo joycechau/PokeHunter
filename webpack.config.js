51ffa4e73ce20a5bd8550a1a4ce0222338922528
Joyce Chau <jyc.chau@gmail.com>
2017-02-19 15:09:37 -0800

    Setup project

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './poke_hunter.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
