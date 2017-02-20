0ad9c29bb9406b1213bc1e0c916d6166be28f662
Joyce Chau <jyc.chau@gmail.com>
2017-02-19 17:18:33 -0800

    Setup Pokemon List (#4)

--- components/pokedex/pokedex.jsx
+++ components/pokedex/pokedex.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import pokemonList from './pokemon_list.js';
 
 class Pokedex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { pokemonList };
   }
 
   render() {
---
+++ components/pokedex/pokemon_list.js
@@ -0,0 +1,16 @@
+export const pokemonList = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    found: false,
+    found_url: "",
+    hidden_url: ""
+  },
+  {
+    id: 2,
+    name: "Ivysaur",
+    found: false,
+    found_url: "",
+    hidden_url: ""
+  }
+];
