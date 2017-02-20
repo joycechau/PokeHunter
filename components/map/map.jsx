26fa60cf32386cf3891cb6e82f63aa14dbfe50c5
Kenneth Truong <kenneth.e.truong@gmail.com>
2017-02-19 23:09:00 -0800

    Add Pokeball Markers

--- components/map/map.jsx
+++ components/map/map.jsx
@@ -1,4 +1,19 @@
 import React from 'react';
+import styles from './map.css';
+
+const POKE_MARKERS = [
+  'https://res.cloudinary.com/joycechau/image/upload/v1487572666/premierball.png',
+  'https://res.cloudinary.com/joycechau/image/upload/v1487572646/ultraball.png',
+  'https://res.cloudinary.com/joycechau/image/upload/v1487572637/safariball.png',
+  'https://res.cloudinary.com/joycechau/image/upload/v1487572623/greatball.png',
+  'https://res.cloudinary.com/joycechau/image/upload/v1487572592/masterball.png',
+  'https://res.cloudinary.com/joycechau/image/upload/v1487572540/pokeball.png'
+];
+
+const MAX_LAT = 49.5;
+const MIN_LAT = 17.5;
+const MAX_LNG = 294.5;
+const MIN_LNG = 236;
 
 class Map extends React.Component {
   constructor(props) {
@@ -6,15 +21,46 @@ class Map extends React.Component {
   }
 
   componentDidMount() {
-    new google.maps.Map(document.getElementById('map'), {
-      center: new google.maps.LatLng(51.508742,-0.120850),
-      zoom: 5,
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      maxZoom: 4,
+      minZoom: 4,
+      center: {lat: 35, lng: 265},
+      zoomControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      rotateControl: false,
+      fullscreenControl: false,
+      panControl: false,
+      draggable: false,
+      scrollwheel: false,
+    });
+
+    for (var i = 0; i < 25; i++) {
+      this.addMarker(map);
+    }
+  }
+
+  addMarker(map) {
+    const lat = Math.random() * (MAX_LAT - MIN_LAT) + MIN_LAT;
+    const lng = Math.random() * (MAX_LNG - MIN_LNG) + MIN_LNG;
+
+    const marker = new google.maps.Marker({
+      position: {lat, lng},
+      icon: POKE_MARKERS[Math.round(Math.random() * 10) % POKE_MARKERS.length],
+      map: map
+    });
+
+    marker.addListener('click', () => {
+      marker.setMap(null);
+      this.addMarker(map);
     });
   }
 
   render() {
     return (
-      <div id="map" style={{ height: '100%'}}/>
+      <div id="map" className={styles.map} />
     );
   }
 }
