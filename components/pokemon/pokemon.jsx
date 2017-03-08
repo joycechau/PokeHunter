d49698eaa68d562b832338fb303e89102aaf6b49
Joyce Chau <jyc.chau@gmail.com>
2017-03-07 22:34:39 -0800

    Add pokemon component

---
+++ components/pokemon/pokemon.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './pokemon.css';
+
+export default class Pokedex extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
