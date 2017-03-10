267e86cb66bbef10e1ade528ed5650b392e12003
Joyce Chau <jyc.chau@gmail.com>
2017-03-09 23:55:14 -0800

    Change map settings

--- components/map/map.jsx
+++ components/map/map.jsx
@@ -33,7 +33,7 @@ export default class Map extends React.Component {
       rotateControl: false,
       fullscreenControl: false,
       panControl: false,
-      draggable: false,
+      draggable: true,
       scrollwheel: false,
     });
 
